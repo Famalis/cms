3e067979866c32dcd7c96e39d3f5429a1f2c03a7
Famalis <segiocosentino1@gmail.com>
2013-11-26 07:45:10 +0100

    Raporty z formularzami

--- CMS/src/java/controllers/reportTemplate/TestReportTemplateController.java
+++ CMS/src/java/controllers/reportTemplate/TestReportTemplateController.java
@@ -4,9 +4,18 @@
  */
 package controllers.reportTemplate;
 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.tool.xml.XMLWorkerHelper;
 import controllers.general.BaseController;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.Map;
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -20,29 +29,54 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 @RequestMapping("/testReportTemplate")
-public class TestReportTemplateController extends BaseController{
-    
+public class TestReportTemplateController extends BaseController {
+
     public TestReportTemplateController() {
         super("all", "ReportsPrint");
     }
-    
+
     @RequestMapping("/testReportTemplate")
     public String home(HttpSession session, ModelMap model,
             HttpServletResponse response, HttpServletRequest request) {
-        if(!this.checkPrivileges(session)) {
+        if (!this.checkPrivileges(session)) {
             return "missingPrivilege";
         }
         Map<String, String[]> inputParams = request.getParameterMap();
         Map<String, Object> params = new HashMap<>();
-        
-        if(inputParams.get("helloMsg")!=null) {
+
+        if (inputParams.get("helloMsg") != null) {
             params.put("helloAttr", inputParams.get("helloMsg")[0]);
-        }        
-        
+        }
         model.addAllAttributes(params);
-        response.setHeader("Content-Disposition", "attachment;filename=report.pdf");        
+        try {
+            ServletContext context = request.getServletContext();
+            String appPath = context.getRealPath("");
+            String filePath = "\\WEB-INF\\templates\\testReportTemplate.html";
+            File sourceFile = new File(appPath + filePath);
+
+            Document document = new Document();
+            //FileOutputStream fos = new FileOutputStream("report.pdf");
+            OutputStream out = response.getOutputStream();
+            PdfWriter writer = PdfWriter.getInstance(document, out);            
+            XMLWorkerHelper worker = XMLWorkerHelper.getInstance();
+            FileInputStream fis = new FileInputStream(sourceFile);
+            worker.parseXHtml(writer, document, fis);
+            response.setHeader("Content-Disposition", "attachment;filename=report.pdf");     
+            document.close();
+            fis.close();
+            out.close();
+            //fos.close();
+            
+            System.out.println(appPath + filePath + "\n" + sourceFile.isFile());
+        } catch (IOException io) {
+
+        } catch (DocumentException docEx) {
+
+        }
+        
+        //response.setHeader("Content-Disposition", "attachment;filename=report.pdf");        
         System.out.println("home");
         return "reportTemplates/testReportTemplate";
-    }     
-    
+    }
+
 }
